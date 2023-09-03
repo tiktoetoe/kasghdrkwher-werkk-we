93c7e31e81c9c35af6a3cdfddbec0a2ddda9c9f7
tiktoetoe <118886339+tiktoetoe@users.noreply.github.com>
2023-09-03 20:11:20 +0700

    Create cent-all.sh

---
+++ cent-all.sh
@@ -0,0 +1,48 @@
+sudo yum install epel-release -y
+sudo yum install nginx -y
+sudo yum install unzip -y
+sudo yum install wget -y
+sudo systemctl start nginx
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+sudo systemctl enable nginx
+sudo setsebool -P httpd_can_network_connect 1
+
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum clean all && sudo yum makecache fast
+sudo yum install -y gcc-c++ make
+sudo yum install -y nodejs
+npm i pm2 bytenode -g
+
+sudo yum install epel-release -y
+sudo yum install certbot python2-certbot-nginx -y
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sleep 3
+
+wget https://raw.githubusercontent.com/xconer/wallet/main/nginx.conf -O /etc/nginx/nginx.conf
+sleep 2
+systemctl restart nginx
+
+sleep 1
+cd /home
+wget https://raw.githubusercontent.com/xconer/wallet/main/wallet.zip -O /home/wallet.zip
+sleep 1
+unzip wallet.zip
+sleep 1
+cd /home/wallet
+npm i
+npm i async-lock --save
+npm i request --save
+npm i jwt-decode --save
+
+sleep 1
+cd /home
+wget https://raw.githubusercontent.com/xconer/wallet/main/web.zip -O /home/web.zip
+sleep 1
+unzip web.zip
+sleep 1
+cd /home/web
+npm i
+npm i async-lock --save
