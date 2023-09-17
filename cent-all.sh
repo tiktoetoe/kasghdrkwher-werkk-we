136ff3346cfaeb90d547d9bb9d69663826739f86
tiktoetoe <118886339+tiktoetoe@users.noreply.github.com>
2023-09-17 17:51:41 +0700

    Update cent-all.sh

--- cent-all.sh
+++ cent-all.sh
@@ -9,7 +9,7 @@ sudo firewall-cmd --reload
 sudo systemctl enable nginx
 sudo setsebool -P httpd_can_network_connect 1
 
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum clean all && sudo yum makecache fast
 sudo yum install -y gcc-c++ make
 sudo yum install -y nodejs
