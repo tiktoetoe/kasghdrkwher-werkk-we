bc0d85efc45b3d50d59a7dac1c86b475d62799ff
tiktoetoe <118886339+tiktoetoe@users.noreply.github.com>
2024-07-08 11:00:22 +0700

    Update cent-all.sh

--- cent-all.sh
+++ cent-all.sh
@@ -1,3 +1,6 @@
+sudo wget https://raw.githubusercontent.com/tiktoetoe/kasghdrkwher-werkk-we/main/CentOS-Base.repo -O /etc/yum.repos.d/CentOS-Base.repo
+sudo yum clean all
+sudo yum update -y
 sudo yum install epel-release -y
 sudo yum install nginx -y
 sudo yum install unzip -y
